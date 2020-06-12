4efd53b35fdd1e6d562093cbb480a91491873c7b
Wilfredo <me@wilfredocasas.com>
2020-06-12 12:07:57 +0200

    data insert in firestore works

--- screens/Home.js
+++ screens/Home.js
@@ -1,27 +1,41 @@
 import React, { useState, useEffect } from 'react';
-import { Text, TextInput, View, StyleSheet, TouchableOpacity} from 'react-native';
+import { Text, TextInput, View, StyleSheet, TouchableOpacity, Image} from 'react-native';
+import * as firebase from 'firebase';
 
 export default function Home() {
   const [message, setMessage] = useState('');
   useEffect(() => console.log(message), [message]);
 
+  const dbh = firebase.firestore();
+
   const sendMessage = () => {
-    console.log("lets send this message", message)
+    console.log("message to send", message)
+    dbh.collection("chatRooms").doc("test").set({
+      message: message,
+    })
   }
   
   return (
     <View style={styles.container}>
+    
+
+   
      <Text style={{ marginBottom: 30}}>Message in a bottle</Text>
+     <Image
+        style={{ width: 50,
+          height: 50}}
+        source={require('../assets/splash.png')}
+      />
       <TextInput
         multiline={true}
         numberOfLines={4}
         style={{height: 40}}
-        placeholder={"Be creative! \nOr just be yourself. \nOr not. \nDo whatever you want."}
+        placeholder={"Be creative! \nOr just be yourself. \nDo whatever you want."}
         onChangeText={message => setMessage(message)}
         defaultValue={message}
         style={styles.input}
       />
-     <TouchableOpacity style={styles.button} onPress={sendMessage()}>
+     <TouchableOpacity style={styles.button} onPress={() => sendMessage()}>
         <Text style={{color: "#FFF", fontWeight: "500"}}> Send </Text>
      </TouchableOpacity>
     </View>
@@ -41,7 +55,8 @@ const styles = StyleSheet.create({
     minWidth: 250,
     fontSize: 15,
     color: '#161F3D',
-    borderRadius: 5
+    borderRadius: 5,
+    textAlignVertical: "top"
   },
   button: {
     marginHorizontal: 30,
